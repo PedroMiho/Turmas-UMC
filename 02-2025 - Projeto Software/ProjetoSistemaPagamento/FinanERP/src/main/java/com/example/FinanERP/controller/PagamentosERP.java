e0d1bea4b6ded1f28742a20bc41e9bdc1d0bb091
PedroMiho <pedromihosoza@gmail.com>
2025-09-30 10:38:41 -0300

    .

--- 02-2025 - Projeto Software/ProjetoSistemaPagamento/FinanERP/src/main/java/com/example/FinanERP/controller/PagamentosERP.java
+++ 02-2025 - Projeto Software/ProjetoSistemaPagamento/FinanERP/src/main/java/com/example/FinanERP/controller/PagamentosERP.java
@@ -1,5 +1,6 @@
 package com.example.FinanERP.controller;
 
+import com.example.FinanERP.model.DadosCadastroPagamento;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class PagamentosERP {
 
     @PostMapping
-    public void cadastrarPagamentos(@RequestBody String pagamento) {
+    public void cadastrarPagamentos(@RequestBody DadosCadastroPagamento pagamento) {
         System.out.println(pagamento);
     }
 }
